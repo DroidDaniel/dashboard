40bc6d4b1dbc25f8a96998be679fa625062eb0a1
Daniel Disowsa <42129480+DroidDaniel@users.noreply.github.com>
2019-12-17 04:27:53 -0800

    Add files via upload

---
+++ js/min.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+       $('#back2Top').fadeIn();
+	    $(".navbar").css({"background":"rgb(41, 41, 41)","transition":"0.5s"});
+	/*	$(".container-fluid").removeClass("border-bottom");*/
+	  }
+
+	  else{
+       $('#back2Top').fadeOut();
+		  $(".navbar").css("background" , "transparent");  	
+		/*  $(".container-fluid").addClass("border-bottom");*/
+	  }
+  });
+
+/**mobile menu animation**/
+  document.getElementById('menu-toggle').addEventListener(
+  'click',
+  function() {
+    this.classList.toggle('menu-toggle-active');
+  }
+);
+
+/**navbar scroll animation**/
+  $('.navbar-toggler').click(function(){
+
+    var hidden = $('.nav-in');
+    if (hidden.hasClass('in')){
+      hidden.removeClass('in');
+    } else {
+      hidden.addClass('in');
+    }
+    });
+
+/**back to top animation**/
+      $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
+
+
+
+
+
+
+ 
